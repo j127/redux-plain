976253f67b99a8ac0547b933db6aae916048e57f
J. Cohen <hostelmarketing@gmail.com>
2020-03-28 20:36:15 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Plain Redux without React
+
+A quick experiment using Redux without React.
+
+```text
+$ npm i
+$ npm start
+```
+
+![cat](./cat.png)
---
+++ data/seeds.js
@@ -0,0 +1,13 @@
+// initial data
+module.exports = [
+    {
+        id: 1,
+        name: "Fluffy",
+        hungry: false,
+    },
+    {
+        id: 2,
+        name: "Santa Claws",
+        hungry: true,
+    },
+];
